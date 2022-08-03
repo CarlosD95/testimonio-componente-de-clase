da68ef98789613c31d497c1c16ae318de87ef79f
CarlosD95 <carlosdaserrano4@gmail.com>
2022-08-03 18:17:28 -0400

    Codigo Nuevo

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import Testimonio from './componentes/Testimonio'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className='contenedor-principal'></div>
+          <h1>Esto es lo que dicen  nuestros alumnos sobre FreeCodeCamp:</h1>
+          <Testimonio 
+            nombre="Shawn Wang"
+            pais="Singapur"
+            cargo="Ingeniera de Software"
+            imagen="emma"
+            empresa="Amazon"
+            testimonio="Siempre tuve miedo de cambiar de carrera"
+          />
+          <Testimonio 
+          nombre="Shawn Wang"
+          pais="Singapur"
+          cargo="Ingeniera de Software"
+          imagen="shawn"
+          empresa="Amazon"
+          testimonio="Siempre tuve miedo de cambiar de carrera"
+          />
+         <Testimonio 
+          nombre="Sarah Chima"
+          pais="Nigeria"
+          cargo="Ingeniera de Software"
+          imagen="sarah"
+          empresa="Chat Desk"
+          negrita= "FreeCodeCamp fue el inicio de mi carrera como programadora"
+          testimonio= ", tiene un buen plan de estudios"
+          />
+      </div>
+    );
+  }
+}
+
+export default App;
